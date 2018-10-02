c8f6b21a273d1ab066e4ca182a7b0f54746c4c25
Sartaj <sartaj@sartaj.me>
2018-10-02 15:01:05 -0500

    add packages

---
+++ contributing.md
@@ -0,0 +1,11 @@
+# Using `npm-workspace`
+
+Use `npm-workspace` to locally link modules.
+
+```bash
+npm i -g npm-workspace
+```
+
+```bash
+npm-workspace install
+```
--- literate-reader/specifications/index.js
+++ literate-reader/specifications/index.js
@@ -3,26 +3,29 @@ const fs = require('fs')
 const path = require('path')
 const concat = require('concat-stream')
 const { pipe } = require('ramda')
-const babel = require('@babel/core')
-const file = path.join(__dirname, 'mocks/itsy-bitsy-data-structures.js')
-var util = require('util');
-const code = fs.readFileSync(file, 'utf8')
+const parser = require('@babel/parser')
+const util = require('util');
 
 const log = obj => {
   console.log(util.inspect(obj, false, null)); // => { code, map, ast }
 }
 
-babel.transform(code, (err, result) => {
-  debugger;
-});
 
-export default function({ types: t}) {
-  return {
-    visitor: {
-      // visitor contents
-    }
-  };
+const options = {
+  options: { 
+    presets: "@babel/env"
+  }
 }
+
+
+
+// export default function({ types: t}) {
+//   return {
+//     visitor: {
+//       // visitor contents
+//     }
+//   };
+// }
 
 // const function (babel) {
 //   const { types: t } = babel;
@@ -37,6 +40,70 @@ export default function({ types: t}) {
 //   };
 // }
 
-// test('', t => {
+test('Should parse js ast', t => {
+  t.plan(1)
+  const file = path.join(__dirname, 'mocks/itsy-bitsy-data-structures.js')
+  const code = fs.readFileSync(file, 'utf8')
+  const ast = parser.parse(code)
+  t.equals(ast.comments.length, 154)
+})
 
+test('Should parse typescript ast', t => {
+  t.plan(1)
+  const file = path.join(__dirname, 'mocks/cyclejs.ts')
+  const code = fs.readFileSync(file, 'utf8')
+  const ast = parser.parse(code, {
+    sourceType: "module",
+    plugins: [
+      "typescript"
+    ]
+  })
+  t.equals(ast.comments.length, 3)
+})
+
+// test('Should parse flow ast', t => {
+//   t.plan(1)
+//   const file = path.join(__dirname, 'mocks/cyclejs.ts')
+//   const code = fs.readFileSync(file, 'utf8')
+//   const ast = parser.parse(code, {
+//     sourceType: "module",
+//     plugins: [
+//       "typescript"
+//     ]
+//   })
+//   t.equals(ast.comments.length, 3)
 // })
+
+
+// test('Should parse jsx ast', t => {
+//   t.plan(1)
+//   const file = path.join(__dirname, 'mocks/cyclejs.ts')
+//   const code = fs.readFileSync(file, 'utf8')
+//   const ast = parser.parse(code, {
+//     sourceType: "module",
+//     plugins: [
+//       "typescript"
+//     ]
+//   })
+//   t.equals(ast.comments.length, 3)
+// })
+
+// Should work with itsy-bitsy style code comments
+
+// Should work with jsdoc style comments
+
+// Should work with markdown annotated code blocks without *
+
+// Should work with markdown annotated code blocks with *
+
+// Should work with markdown annotated single line blocks
+
+// Should link imports
+
+// Should be able to have annotation popups
+
+// Should work with regular markdown
+
+// Links in markdown to other files should work
+
+
